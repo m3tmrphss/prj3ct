25f4f25fe5330f6ab9232ea4a24e5f50895da97c
m3tmrphss <125642850+m3tmrphss@users.noreply.github.com>
2024-04-03 00:46:23 +0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+let  adaptiveContainer = document.querySelector(`#adaptive-container`);
+let adaptiveLink = document.querySelector(`.header__adaptive-link`);
+adaptiveLink.addEventListener(`click`, function(){
+    adaptiveContainer.classList.toggle(`active`)
+})
+
+ 
